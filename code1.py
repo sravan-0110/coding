25e5e166a4c13078d507968c103e982ed8f5fbf4
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-20 22:33:33 +0530

    againadded

--- code1.py
+++ code1.py
@@ -4,15 +4,10 @@ bfl,iauefb.AEHF
 print("Front side counting:")
 for i in range(1, n + 1):
     print(i, end=' ')
-    print()  # Line break
-
-
+    print()  @@@@@@@@@@@@@@@@@@@@@@
 
 print("Back side counting:")
 for i in range(n, 0, -1):
     print(i, end=' ')
     print() 
-    
-    hcakuigdfhliuaEwhdfbef
-    12345@@@
-    sd
+
